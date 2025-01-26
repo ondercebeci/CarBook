128114dceb7265f591ab9920d770d570aec1f2ec
ondercebeci <69725787+ondercebeci@users.noreply.github.com>
2025-01-26 19:33:49 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/BannerHandlers/UpdateBannerCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Features.CQRS.Commands.BannerCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers
+{
+	public class UpdateBannerCommandHandler
+	{
+		private readonly IRepository<Banner> _repository;
+
+		public UpdateBannerCommandHandler(IRepository<Banner> repository)
+		{
+			_repository = repository;
+		}
+		public async Task Handle(UpdateBannerCommand command)
+		{
+			var values = await _repository.GetByIdAsync(command.BannerID);
+			values.VideoDescription = command.VideoDescription;
+			values.Description = command.Description;
+			values.VideoUrl = command.VideoUrl;
+			values.Title = command.Title;
+			await _repository.UptdateAsync(values);
+		}
+	}
+}
---
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -0,0 +1,55 @@
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Persistence.Context;
+using UdemyCarBook.Persistence.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddScoped<CarBookContext>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
