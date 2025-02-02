0f39ef625f1b533169bc74b61b0576eed2dfccb8
ondercebeci <69725787+ondercebeci@users.noreply.github.com>
2025-02-02 17:37:30 +0300

    Web UI işlemleri

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commands.SocialMediaCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+    public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.SocialMediaID);
+            value.SocialMediaID = request.SocialMediaID;
+            value.Url = request.Url;
+            value.Icon = request.Icon;
+            value.Name = request.Name;
+            await _repository.UptdateAsync(value);
+        }
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commands.TestimonialCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class UpdateTestimonialCommandHandler : IRequestHandler<UpdateTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public UpdateTestimonialCommandHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.TestimonialID);
+            value.TestimonialID = request.TestimonialID;
+            value.Comment = request.Comment;
+            value.Name = request.Name;
+            value.Title = request.Title;
+            value.ImageUrl = request.ImageUrl;
+            await _repository.UptdateAsync(value);
+        }
+    }
+}
